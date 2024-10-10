b44d70542e1c939fe69eb3e2be70dd01fa792f36
danielvramos <vieira.ramos@gmail.com>
2024-10-10 09:51:30 -0300

    vai porra

--- src/analisador_lexeco.c
+++ src/analisador_lexeco.c
@@ -1,9 +1,20 @@
-#include <stdio.h>
+
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#define MAX_TOKENS 500
 #define MAX_TOKEN_LENGTH 100
+#define MAX_CHILDREN 10
+
+/******************************************************************************
+
+                        DECLARAÇÕES
+
+*******************************************************************************/
+
+
+
 
 // Enumeração dos tipos de tokens
 typedef enum {
@@ -34,6 +45,24 @@ const char* palavras_chave[] = {
 };
 int num_palavras_chave = 10;
 
+typedef struct node 
+{ 
+    char* info;  
+    int type;
+    int childCount;  
+    struct node* children[MAX_CHILDREN];  
+    
+}Node;
+
+
+/******************************************************************************
+
+                        METODOS E FUNÇÕES DO ANALISADOR LÉXICO
+
+*******************************************************************************/
+
+
+
 // Função que verifica se o lexema é uma palavra-chave
 int eh_palavra_chave(const char* lexema) {
     for (int i = 0; i < num_palavras_chave; i++) {
@@ -52,6 +81,8 @@ Token classificar_token(const char* lexema, int linha) {
     if (eh_palavra_chave(lexema)) {
         token.tipo = PALAVRA_CHAVE;
         contador_palavras_chave++;
+     
+
     } else if (strcmp(lexema, "&&") == 0 || strcmp(lexema, "||") == 0) {
         token.tipo = OPERADOR_LOGICO;
         contador_operadores++;
@@ -103,12 +134,21 @@ void exibir_token(Token token) {
     printf("%-15s : %s\n", tipo_token[token.tipo], token.lexema);
 }
 
+void inserirToken(Token *ListaTokens, Token novoToken){
+        int i = 0;  
+    while (ListaTokens[i].tipo != NULL)  
+        i++;
+    ListaTokens[i] = novoToken;      
+}
+
+
 // Função que analisa o código de entrada e identifica os tokens
-void analisar_lexico(const char* codigo, int linha) {
+Token * analisar_lexico(const char* codigo, int linha) {
     int i = 0;
     char lexema[MAX_TOKEN_LENGTH];
     int pos_lexema = 0;
-
+    Token ListaTokens[MAX_TOKENS];
+    
     while (codigo[i] != '\0') {
         // Ignora espaços em branco
         if (isspace(codigo[i])) {
@@ -124,6 +164,7 @@ void analisar_lexico(const char* codigo, int linha) {
             }
             lexema[pos_lexema] = '\0';
             Token token = classificar_token(lexema, linha);
+            inserirToken(ListaTokens, token);
             exibir_token(token);
         }
         // Identifica números inteiros ou reais
@@ -140,6 +181,7 @@ void analisar_lexico(const char* codigo, int linha) {
             }
             lexema[pos_lexema] = '\0';
             Token token = classificar_token(lexema, linha);
+            inserirToken(ListaTokens, token);
             exibir_token(token);
         }
         // Identifica operadores, delimitadores e símbolos
@@ -165,9 +207,11 @@ void analisar_lexico(const char* codigo, int linha) {
             }
 
             Token token = classificar_token(lexema, linha);
+            inserirToken(ListaTokens, token);
             exibir_token(token);
         }
     }
+    return ListaTokens;
 }
 
 // Função para exibir o resumo dos tokens
@@ -181,6 +225,103 @@ void exibir_resumo_tokens() {
     printf("Delimitadores     : %d\n", contador_delimitadores);
     printf("Tokens Desconhecidos : %d\n", contador_token_desconhecido);
 }
+
+/******************************************************************************
+
+                        FUNC E METODOS DO ANALISADOR SINTÁTICO
+
+*******************************************************************************/
+
+Node* CreateNode(char *value, int ntype)  
+{
+    Node* p = (Node*)malloc(sizeof(Node));  
+    int i;  
+    p->info = value;  
+    p->type = ntype;  
+    for (i = 0; i < MAX_CHILDREN; i++)  
+        p->children[i] = NULL;  return p;  
+    
+}
+Node* AddChild(Node* node, char *value, int ntype)  
+{
+    int i = 0;  
+    while (node->children[i] != NULL)  
+        i++;  
+    if (i < MAX_CHILDREN)  
+        node->children[i] = CreateNode(value, ntype);  
+    else  
+        printf("Error: MAX_CHILDREN!\n");  
+    return node->children[i];  }
+
+
+void PrintTree(Node *node, int tabs)  
+{
+    int i = 0;  
+    int x = 0;  
+    for (x = 0; x < tabs; x++)  
+        printf("\t");  printf("%s\n", node->info);  
+    while (node->children[i] != NULL)  
+    {
+        PrintTree(node->children[i], tabs + 1);
+        i++;  
+        
+    }
+    
+}
+
+
+Node* buildDeclarationTree(char** tokens, int tokenCount) {
+    Node* root = createNode(tokens[0], "TYPE"); // Assume que o primeiro token é o tipo
+    Node* current = root;
+    int i = 1;
+
+    while (i < tokenCount) {
+        if (strcmp(tokens[i], "=") == 0) {
+            // Encontrou uma atribuição
+            Node* assignment = AddChild(current,"=", "OPERATOR");
+            current = assignment;
+            i++; // Avança para a expressão
+        } else if (strcmp(tokens[i], ";") == 0) {
+            // Fim da declaração
+            break;
+        } else {
+            // Adiciona um novo filho (identificador ou expressão)
+            Node* newNode = createNode(tokens[i], "IDENTIFIER"); // Assume que o restante são identificadores
+            current->children[current->childCount++] = newNode;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+/*
+int termo(FILE *code_file, int next_token, NextChar *next_char,  Node *tree)
+{
+    printf("Enter <termo>\n");  
+    Node *termonode = AddChild(tree, "termo", -1); 
+    next_token = fator(code_file, next_token, next_char, termonode);
+    while (next_token == MULT_OP || next_token == DIV_OP)  
+    {
+        if (next_token == MULT_OP)  
+            AddChild(termonode, "*", next_token);  
+        else 
+            if (next_token == DIV_OP)  
+            AddChild(termonode, "/", next_token);
+        next_token = lex(code_file, next_char);
+        next_token = fator(code_file, next_token, next_char, termonode);  
+        
+    }
+    printf("Exit <termo>\n");
+    return next_token;  
+
+}*/
+
+/******************************************************************************
+
+                      FUNÇÃO MAIN
+
+*******************************************************************************/
 
 // Função principal que recebe o caminho do arquivo e chama o analisador léxico
 int main(int argc, char *argv[]) {
@@ -206,7 +347,9 @@ int main(int argc, char *argv[]) {
         int linha_atual = 1;
         printf("Lendo o arquivo %s...\n\n", filepath);
         while (fgets(linha, sizeof(linha), arquivo)) {
-            analisar_lexico(linha, linha_atual);
+            Token *ListaTokens;
+
+            ListaTokens = analisar_lexico(linha, linha_atual);
             linha_atual++;
         }
 
